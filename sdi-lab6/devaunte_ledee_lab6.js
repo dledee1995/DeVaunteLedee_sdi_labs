0e6aa0190c2b6bf685ecd69c4e773c6a00c75d58
DeVaunte Ledee <devaunteledee@DeVauntes-MacBook-Pro.local>
2014-05-17 14:59:24 -0400

    adding for loop

--- sdi-lab6/devaunte_ledee_lab6.js
+++ sdi-lab6/devaunte_ledee_lab6.js
@@ -4,10 +4,20 @@ var myFunction = function() {
 	// Variables
 	var nflTeams = ["Giants", "Panthers", "Broncos", "Ravens"];
 	var teamHomes = ["NewYork", "Carolina", "Denver", "Baltimore"];
-	var newTeam = "Arawaks";
-	var newHome = "Orlando";
 
-	console.log(nflTeams);
+	
+	//for loop
+	for (var i = 0; i < nflTeams.length; i++) {
+
+		console.log("The Football team known as the " + nflTeams[i] + " is located in " + teamHomes[i]);
+
+		// };
+
+	};
+	// return for array push
+	
+
+	//console.log(newfirstName);
 
 };
 myFunction();
