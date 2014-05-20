501b7cdac75996bb28c9957f103cc4df18c10a0d
DeVaunte Ledee <devaunteledee@DeVauntes-MacBook-Pro.local>
2014-05-20 15:56:05 -0400

    fourth commit

--- sdi-lab7/rename-me.js
+++ sdi-lab7/rename-me.js
@@ -16,6 +16,10 @@ var JordanFunctions = function() {
 			firstJordan = new objconstructor(jsonData.Jordans[i].make, jsonData.Jordans[i].model, jsonData.Jordans[i].year, jsonData.Jordans[i].release);
 			console.log(firstJordan);
 			console.log(" My first pair of sneakers where called the " + firstJordan.model + ". They are known as " + firstJordan.make + " which originally came out in " + firstJordan.release + " then got re-released in " + firstJordan.year );
+		}else {
+			secondJordan = new objconstructor(jsonData.Jordans[i].make, jsonData.Jordans[i].model, jsonData.Jordans[i].year, jsonData.Jordans[i].release);
+		console.log(secondJordan);
+		console.log(" The sneakers Micheal Jordan wore in the movie Space jam are known as " + secondJordan.make + " the official name of the shoes are " + secondJordan.model + ". The shoes originally were released in " + secondJordan.release + " then the shoes re-released in " + secondJordan.year);
 		}
 
 	};
