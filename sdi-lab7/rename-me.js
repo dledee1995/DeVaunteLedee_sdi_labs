17644ade79200857b87deb8a3780111d7d355444
DeVaunte Ledee <devaunteledee@DeVauntes-MacBook-Pro.local>
2014-05-20 15:55:08 -0400

    Second commit

--- sdi-lab7/rename-me.js
+++ sdi-lab7/rename-me.js
@@ -8,5 +8,15 @@ var objconstructor = function(jordanMake,jordanModel,jordanYear, jordanRelease) 
 	this.release = jordanRelease;
 };
 
+var JordanFunctions = function() {
 
+	for (var i = 0; i < jsonData.Jordans.length; i++) {
+
+	
+
+	};
+
+};
+
+JordanFunctions();
 
