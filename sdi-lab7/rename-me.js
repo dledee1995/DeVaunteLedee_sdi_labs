0e90347646813418d34007e587f5d50f72494836
DeVaunte Ledee <devaunteledee@DeVauntes-MacBook-Pro.local>
2014-05-20 15:55:44 -0400

    third commit

--- sdi-lab7/rename-me.js
+++ sdi-lab7/rename-me.js
@@ -12,7 +12,11 @@ var JordanFunctions = function() {
 
 	for (var i = 0; i < jsonData.Jordans.length; i++) {
 
-	
+		if (i === 0) {
+			firstJordan = new objconstructor(jsonData.Jordans[i].make, jsonData.Jordans[i].model, jsonData.Jordans[i].year, jsonData.Jordans[i].release);
+			console.log(firstJordan);
+			console.log(" My first pair of sneakers where called the " + firstJordan.model + ". They are known as " + firstJordan.make + " which originally came out in " + firstJordan.release + " then got re-released in " + firstJordan.year );
+		}
 
 	};
 
