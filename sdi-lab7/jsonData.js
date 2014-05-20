0aff267a81eab775300deaf83225c66361f14b56
DeVaunte Ledee <devaunteledee@DeVauntes-MacBook-Pro.local>
2014-05-20 15:54:11 -0400

    First commit

---
+++ sdi-lab7/jsonData.js
@@ -0,0 +1,35 @@
+var jsonData = {
+	
+	"Jordans":
+	[
+		{
+			
+			"make": "Retro 7s",
+			"model": "Bordeauxs",
+			"year" : 2011,
+			"release": 1992
+			
+		},
+		{
+			
+			"make": "Retro 11s",
+			"model": "Space Jams",
+			"year" : 2009,
+			"release": 2000,
+			
+		},
+		
+		{
+			
+			"make": "Retro 9s",
+			"model": "Chicagos",
+			"year" : 2010,
+			"release": 1993
+			
+		}
+	
+	
+	
+	]	
+	
+};
---
+++ sdi-lab7/rename-me.js
@@ -0,0 +1,12 @@
+//alert("JavaScript works!");
+
+
+var objconstructor = function(jordanMake,jordanModel,jordanYear, jordanRelease) {
+	this.make = jordanMake;
+	this.model = jordanModel;
+	this.year = jordanYear;
+	this.release = jordanRelease;
+};
+
+
+
