ce124ae83fe09d56d9c1ceb87f405aa5cfce8f62
DeVaunte Ledee <devaunteledee@DeVauntes-MacBook-Pro.local>
2014-05-22 15:00:59 -0400

    1st commit

---
+++ sdi-lab8/devaunte_ledee_lab8.js
@@ -0,0 +1,56 @@
+
+//  Global Variables
+var testEmail = "dledee1995@gmail.com";
+// Function
+var emailFunction = function(testEmail) {
+	
+	// Local Variables
+	var atPosition = testEmail.lastIndexOf("@");
+	var atSign = testEmail.indexOf("@");
+	var noSpace = testEmail.indexOf(" ");
+	var dot = testEmail.indexOf(".");
+
+// if statements / else
+	if (noSpace > 0) {
+		console.log("Please take out space");
+
+	} else {
+
+		console.log("You have entered your email correctly with no spaces. Welcome!");
+	}
+
+	if (dot < 2) {
+		console.log("Please take out . ");
+
+	} else {
+
+		console.log("You have entered the corrent amount of . signs. Welcome!");
+	}
+
+	if (atPosition > atSign) {
+		console.log(" Please do not use more than one @ symbols");
+	} else {
+		console.log("You have the corrent amount of @ signs. Welcome!");
+	}
+
+	if (atSign < -1) {
+
+		console.log("Please use an @ symbol");
+
+	} else {
+
+		console.log("You have the corrent amount of @ signs. Welcome!");
+	}
+
+};
+// Calling the function
+emailFunction(testEmail);
+
+
+
+
+
+
+
+
+
