bc296e27dec0d9f9cf1e39d2f3fce1910eb57769
DeVaunte Ledee <devaunteledee@DeVauntes-MacBook-Pro.local>
2014-05-22 15:01:31 -0400

    1st commit

--- sdi-lab8/devaunte_ledee_lab8.js
+++ sdi-lab8/devaunte_ledee_lab8.js
@@ -10,37 +10,6 @@ var emailFunction = function(testEmail) {
 	var noSpace = testEmail.indexOf(" ");
 	var dot = testEmail.indexOf(".");
 
-// if statements / else
-	if (noSpace > 0) {
-		console.log("Please take out space");
-
-	} else {
-
-		console.log("You have entered your email correctly with no spaces. Welcome!");
-	}
-
-	if (dot < 2) {
-		console.log("Please take out . ");
-
-	} else {
-
-		console.log("You have entered the corrent amount of . signs. Welcome!");
-	}
-
-	if (atPosition > atSign) {
-		console.log(" Please do not use more than one @ symbols");
-	} else {
-		console.log("You have the corrent amount of @ signs. Welcome!");
-	}
-
-	if (atSign < -1) {
-
-		console.log("Please use an @ symbol");
-
-	} else {
-
-		console.log("You have the corrent amount of @ signs. Welcome!");
-	}
 
 };
 // Calling the function
