c8b4dfd3292476d8e2484f36961f41ece1514409
DeVaunte Ledee <devaunteledee@DeVauntes-MacBook-Pro.local>
2014-05-22 15:25:31 -0400

    part 2 1st commit

--- sdi-lab8/devaunte_ledee_lab8.js
+++ sdi-lab8/devaunte_ledee_lab8.js
@@ -1,56 +1,52 @@
-
 //  Global Variables
-var testEmail = "dledee1995@gmail.com";
-// Function
-var emailFunction = function(testEmail) {
-	
-	// Local Variables
-	var atPosition = testEmail.lastIndexOf("@");
-	var atSign = testEmail.indexOf("@");
-	var noSpace = testEmail.indexOf(" ");
-	var dot = testEmail.indexOf(".");
+/*var testEmail = "dledee1995@gmail.com";
+ // Function
+ var emailFunction = function(testEmail) {
 
-// if statements / else
-	if (noSpace > 0) {
-		console.log("Please take out space");
+ // Local Variables
+ var atPosition = testEmail.lastIndexOf("@");
+ var atSign = testEmail.indexOf("@");
+ var noSpace = testEmail.indexOf(" ");
+ var dot = testEmail.indexOf(".");
 
-	} else {
+ // if statements / else
+ if (noSpace > 0) {
+ console.log("Please take out space");
 
-		console.log("You have entered your email correctly with no spaces. Welcome!");
-	}
+ } else {
 
-	if (dot < 2) {
-		console.log("Please take out . ");
+ console.log("You have entered your email correctly with no spaces. Welcome!");
+ }
 
-	} else {
+ if (dot < 2) {
+ console.log("Please take out . ");
 
-		console.log("You have entered the corrent amount of . signs. Welcome!");
-	}
+ } else {
 
-	if (atPosition > atSign) {
-		console.log(" Please do not use more than one @ symbols");
-	} else {
-		console.log("You have the corrent amount of @ signs. Welcome!");
-	}
+ console.log("You have entered the corrent amount of . signs. Welcome!");
+ }
 
-	if (atSign < -1) {
+ if (atPosition > atSign) {
+ console.log(" Please do not use more than one @ symbols");
+ } else {
+ console.log("You have the corrent amount of @ signs. Welcome!");
+ }
 
-		console.log("Please use an @ symbol");
+ if (atSign < -1) {
 
-	} else {
+ console.log("Please use an @ symbol");
 
-		console.log("You have the corrent amount of @ signs. Welcome!");
-	}
+ } else {
 
-};
-// Calling the function
-emailFunction(testEmail);
+ console.log("You have the corrent amount of @ signs. Welcome!");
+ }
 
-
+ };
+ // Calling the function
+ emailFunction(testEmail);*/
 
 
-
-
-
-
-
+//  Variables
+var mySplit = "fork,spoon,spork";
+var b = ",";
+var c = "/";
